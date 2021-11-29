362f0ffb569e9c09e058507bf8ef7f2fbdfde42f
matiasnaakka <matias5230@gmail.com>
2021-11-29 12:46:54 +0200

    muutokset

---
+++ reitti.js
@@ -0,0 +1,31 @@
+const apiRoot = 'https://api.digitransit.fi/routing/v1/routers/hsl';
+
+const getRoutes = async () => {
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+  console.log('params', { from, to })
+
+  try {
+    const response = await fetch(`${apiRoot}/index/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'query': `{
+         stop(id: \"HSL:1173434\") {
+           name
+           lat
+           lon
+         }
+      }`,
+      }),
+    });
+
+    console.log('response', response);
+    const responseJson = await response.json();
+    console.log('responseJson', responseJson);
+  } catch (e) {
+    console.error('Failed to fetch routes', e);
+  }
+};
