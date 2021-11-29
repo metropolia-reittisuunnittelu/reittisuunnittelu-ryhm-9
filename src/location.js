4f9f7e3f9a2a16f5bb5fc40c24e126479f898df2
Eric Askari <eric.askari@weare.fi>
2021-11-29 16:15:59 +0200

    get user location

---
+++ src/location.js
@@ -0,0 +1,33 @@
+import { alert } from './alert/alert';
+
+function getLocation() {
+    console.log('getLocation')
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((showPosition) => {
+            console.log(showPosition)
+            alert(JSON.stringify(showPosition.coords.accuracy, undefined, 3));
+        }, (err) => {
+            alert(err);
+            console.log(err)
+        }, {});
+    } else {
+        console.log('Geolocation is not supported by this browser');
+    }
+}
+
+function showPosition(position) {
+    console.log(position);
+}
+
+const getLocationButton = document.getElementById('get-location-button');
+console.log(getLocationButton);
+
+getLocationButton.onclick = async () => {
+    console.log('get-location-button')
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log('Geolocation is not supported by this browser');
+    }
+    getLocation();
+};
