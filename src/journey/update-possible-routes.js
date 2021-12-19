2a8aa5f102f615af387a34bd3e2b994c87fd5fed
Mika Kivimäki <mikivima@hotmail.com>
2021-12-19 21:07:43 +0200

    Added departure time for rides

--- src/journey/update-possible-routes.js
+++ src/journey/update-possible-routes.js
@@ -18,27 +18,34 @@ function getWalkElement(step) {
 
 function getBusElement(step) {
     console.log('bus step: ', step);
-    return `<div class="possible-route-step" style="background-color:cadetblue;text-align: center">${ step.mode } ${ step.trip.route.shortName }
+    return `<div class="possible-route-step" style="background-color:cadetblue;text-align: center">
+    ${ step.mode } ${ step.trip.route.shortName }
             <img src="./images/bus.png" alt="bus-logo" style="margin:10px;width:80px;display:flex;">
             <div class="startTime" style="background-color: white;alignment-baseline: center;"> Lähtee: ${ getFormattedTime(step) }</div></div>`
 }
 
 function getTramElement(step) {
     console.log('tram step: ', step);
-    return `<div class="possible-route-step-tram" style="background-color:greenyellow;text-align: center">${ step.mode } ${ step.trip.route.shortName }
-    <img src="./images/ratikka.png" alt="tram-logo" style="width:80px"></div>`
+    return `<div class="possible-route-step-tram" style="background-color:greenyellow;text-align: center">
+    ${ step.mode } ${ step.trip.route.shortName }
+    <img src="./images/ratikka.png" alt="tram-logo" style="width:80px">
+    <div class="startTime" style="background-color: white;alignment-baseline: center;"> Lähtee: ${ getFormattedTime(step) }</div></div>`
 }
 
 function getRailElement(step) {
     console.log('rail step: ', step);
-    return `<div class="possible-route-step" style="background-color:indianred;text-align: center">${ step.mode } ${ step.trip.route.shortName}
-    <img src="./images/rail.png" alt="rail-logo" style="width:80px"></div>`
+    return `<div class="possible-route-step" style="background-color:indianred;text-align: center">
+    ${ step.mode } ${ step.trip.route.shortName}
+    <img src="./images/rail.png" alt="rail-logo" style="width:80px">
+    <div class="startTime" style="background-color: white;alignment-baseline: center;"> Lähtee: ${ getFormattedTime(step) }</div></div>`
 }
 
 function getSubwayElement(step) {
     console.log('subway step: ', step);
-    return `<div class="possible-route-step" style="background-color:orange;text-align: center">${ step.mode } ${ step.trip.route.shortName}
-    <img src="./images/subway.png" alt="subway-logo" style="margin:10px;width:80px;display:flex"></div>`
+    return `<div class="possible-route-step" style="background-color:orange;text-align: center">
+    ${ step.mode } ${ step.trip.route.shortName}
+    <img src="./images/subway.png" alt="subway-logo" style="margin:10px;width:80px;display:flex">
+    <div class="startTime" style="background-color: white;alignment-baseline: center;"> Lähtee: ${ getFormattedTime(step) }</div></div>`
 }
 
 export async function drawPossibleRoutes() {
